ec279b879388fc0bed969760a44be628bec29b22
cloudy <im@joe.cloud>
2020-05-21 20:45:04 -0500

    Record trajectories into csvs

---
+++ test_n_joint_arm_to_point_control.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+#sys.path.append(os.path.dirname(__file__) + "/../ArmNavigation/n_joint_arm_to_point_control/")
+
+import armnavigation.n_joint_arm_to_point_control.n_joint_arm_to_point_control as m
+
+
+m.show_animation = True
+m.animation()
+
