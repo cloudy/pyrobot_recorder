7c11b7954d7aa11b2cc47e1b05883405d21bff0d
cloudy <im@joe.cloud>
2020-07-22 18:00:10 -0500

    Add argparse for loading trajectories

--- playback_n_joint_arm.py
+++ playback_n_joint_arm.py
@@ -1,12 +1,18 @@
 import os
 import sys
+import argparse
 import numpy as np
 
 #sys.path.append(os.path.dirname(__file__) + "/../ArmNavigation/n_joint_arm_to_point_control/")
 
 import armnavigation.n_joint_arm_to_point_control.n_joint_arm_to_point_control as m
 
-data_f = 'armnavigation/n_joint_arm_to_point_control/trajectories/traj-0-test.csv'
+default_dir = 'armnavigation/n_joint_arm_to_point_control/trajectories/traj-0-test.csv'
+parser = argparse.ArgumentParser()
+parser.add_argument('--file', help='File path', default=default_dir)
+args = parser.parse_args()
+
+data_f = args.file
 data = np.loadtxt(data_f, delimiter=',')
 
 m.show_animation = True
